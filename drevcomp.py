21d14f215a06d502c4ba9439d16dc85bca05f694
duanjingqi <29846423+duanjingqi@users.noreply.github.com>
2017-10-11 11:39:57 -0400

    Add files via upload

---
+++ drevcomp.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# Usage: drevcomp.py seq1.fa seq2.fa ... seqN.fa
+# create by Jingqi
+# It can take more than one fasta file. 
+
+import sys
+import os
+
+# The Nucleotide complementary pair
+complement_Nt={"A":"T", "T":"A", "C":"G", "G":"C", "U":"A", "N":"N"}
+
+# The function to tailor the fasta file
+def fasta_dict(input_file):
+    """fasta_dict takes single fasta file and joins the sequence lines under a specific head; outputs a dictionary."""
+    fasta_in = open(input_file, 'r').read().strip().split('\n')
+    single_lined_fasta_dict = {}
+    for line in fasta_in: 
+        # Avoid the empty line
+        if len(line) > 0: 
+            # fasta head
+            if line[0] == '>':
+                fasta_head = line
+                single_lined_fasta_dict[fasta_head] = ''
+            else: 
+                # join the sequences in separate rows, have them in upper case.
+                single_lined_fasta_dict[fasta_head] += line.upper()
+    return single_lined_fasta_dict
+
+# The function doing reverse/complement
+def revcomp_dict(head, seq): 
+    """revcomp_dict takes a single fasta (one head + one seq) and does reverse/complement; outputs a dictionary of single item."""
+    revcomp_dict = {}
+    # rename the fasta header for the r/c fasta
+    head_rc = head[0] + "RC_" + head[1:]
+    # reverse the sequence and fetch the complementary Nt
+    seq_rc = "".join([complement_Nt[seq[-i]] for i in range(1,len(seq)+1)])
+    # make a dictionary
+    revcomp_dict[head_rc] = seq_rc
+
+    return revcomp_dict
+
+# Loop through input
+files = sys.argv[1:]
+for afile in files: 
+    # Reverse/complement the input file
+    fasta_input = fasta_dict(afile)
+    fasta_output = {}
+    for head,seq in fasta_input.items(): 
+        fasta_output.update(revcomp_dict(head,seq))
+
+    # Write the output
+    file_name = afile.split(".")[0] + ".rcfa"
+    file_body = ""
+    for head,seq in fasta_output.items():
+        file_body += "{}\n{}\n".format(head,seq)
+    with open(file_name,'w') as f:
+        f.write(file_body)
+
+# END
